3d5da231d15b08b7e776d90776e650d7f128247f
igmerwina <igmerwina@gmail.com>
2019-04-24 21:44:31 +0700

    bab12.6-Switch-if-else-style.go

---
+++ bab12.6-Switch-if-else-style.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	point := 6
+
+	switch {
+	case point == 8:
+		fmt.Println("perfect")
+	case (point < 8) && (point > 3):
+		fmt.Println("awesome")
+	default:
+		{
+			fmt.Println("not bad sapi")
+			fmt.Println("you need to learn more babi")
+		}
+	}
+}
--- bab6-hello-world.go
+++ bab6-hello-world.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	sapi, bebek := "sapi", "bebek"
-	// var firstName string = "Erwin"
-	// lastName, age := "Ardiantha", 27
-	fmt.Println(sapi + " " + bebek)
-	// fmt.Printf("Hello %s %s %d!\n", firstName, lastName, age)
+	var firstName string = "Erwin"
+	lastName, age := "Ardiantha", 27
+	fmt.Printf("Hello %s %s %d!\n", firstName, lastName, age)
 }
