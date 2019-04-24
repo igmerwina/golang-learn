40b328368806d7e5efe68cea7e3eab67aab6e94c
igmerwina <igmerwina@gmail.com>
2019-04-24 23:01:52 +0700

    bab14-array.go

---
+++ bab14-array.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// --- cara 1 ---
+	// var fruits = [4]string{"apple", "grape", "banana"}
+
+	// fmt.Println("Jumlah elemen: \t\t", len(fruits))
+	// fmt.Println("Isi semua elemen: \t", fruits)
+
+	// --- cara 2 --- | Array tanpa jumlah elemen
+	// var angkas = [...]string{"sapi", "bebek"}
+
+	// fmt.Println(angkas)
+
+	// --- cara 3 --- | Array multidimensi
+	// var angkas1 = [2][3]int{[3]int{3, 2, 3}, [3]int{3, 4, 5}}
+	// var angkas2 = [2][3]int{{3, 2, 3}, {3, 4, 5}}
+
+	// fmt.Println("angkas1", angkas1)
+	// fmt.Println("angkas2", angkas2)
+
+	// --- cara 4 --- | range
+	var buahs = [3]string{"apel", "pisang", "kinca"}
+
+	for i, buah := range buahs {
+		fmt.Printf("elemen ke: %d %s\n", i, buah)
+	}
+
+}
