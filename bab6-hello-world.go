e9fe7c4e27243953ff1df4fc66a9231ebc8cf4d1
igmerwina <igmerwina@gmail.com>
2019-04-24 21:35:14 +0700

    bab12-if-else-var-temporary

--- bab6-hello-world.go
+++ bab6-hello-world.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	sapi, bebek := "sapi", "bebek"
+	// var firstName string = "Erwin"
+	// lastName, age := "Ardiantha", 27
+	fmt.Println(sapi + " " + bebek)
+	// fmt.Printf("Hello %s %s %d!\n", firstName, lastName, age)
 }
