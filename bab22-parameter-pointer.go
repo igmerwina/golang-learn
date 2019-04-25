b1b53e8e14db41850aedc705b6921666f99d1f13
igmerwina <igmerwina@gmail.com>
2019-04-25 21:53:27 +0700

    bab22-parameter-pointer.go

---
+++ bab22-parameter-pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// -- parameter pointer --
+func main() {
+	var num = 4
+	fmt.Println(num)
+
+	change(&num, 10)
+	fmt.Println(num)
+}
+
+func change(original *int, value int) {
+	*original = value // parameter pointer
+}
+
+// -- print nilai & alamat
+/*
+func main() {
+	var a int = 10
+	var sapi *int = &a
+	fmt.Println(a)    // print nilai
+	fmt.Println(sapi) // print alamat
+} */
