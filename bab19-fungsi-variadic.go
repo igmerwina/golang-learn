15a9a2a7dedd96223aa9066c5d13595110d89e7f
I Gusti Made Erwin A <igmerwina@gmail.com>
2020-04-23 21:41:44 +0700

    bab19 - menambah penjelasan fungsi variadic di komen

--- bab19-fungsi-variadic.go
+++ bab19-fungsi-variadic.go
@@ -2,19 +2,24 @@ package main
 
 import "fmt"
 
+/*
+	fungsi variadic = fungsi yang pake parameter sejenis yang tak terbatas
+	kaya contoh fungsi(angka, angka, angka..... ~)
+*/
+
 func mainFuncVariadic() {
-	var avg = calculate(2, 3, 4, 2, 2, 5, 2, 1)
-	var msg = fmt.Sprintf("Rata-rata : %.2f", avg)
+	var avg = hitung(2, 1, 2, 1, 2, 1, 2)
+	var msg = fmt.Sprintf("%.2f", avg)
 	fmt.Println(msg)
 }
 
 // fungsi variadic kena ...
-func calculate(numbers ...int) float64 {
-	var total int = 0
+func hitung(numbers ...int) float64 {
+	jumlah := 0
 	for _, number := range numbers {
-		total += number
+		jumlah += number
 	}
 
-	var avg = float64(total) / float64(len(numbers))
+	var avg = float64(jumlah) / float64(len(numbers))
 	return avg
 }
